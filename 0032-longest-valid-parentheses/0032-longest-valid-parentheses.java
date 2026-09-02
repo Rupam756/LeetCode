50035cabcbeb6c4615db41764f11e516a688229f
RUPAM MONDAL <mondalrupam756@gmail.com>
2026-09-02 18:40:28 +0530

    Time: 5 ms (79.77%), Space: 46.5 MB (59.86%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if (ch == '('){
+                stack.push(i);
+            }
+
+            else{
+                stack.pop();
+
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }else{
+                    int length = i - stack.peek();
+
+                    maxLength = Math.max(maxLength, length);
+                }
+            }
+        }
+        return maxLength;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
